5b4d1454721c4a65fc2c132b314cf9f01149f7c4
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-07-14 11:56:52 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Sidebar from './components/Sidebar';
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Topbar from './components/Topbar';
+import Dashboard from './components/Dashboard';
+import Table from './components/Table';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Topbar />
+            <div class="container-fluid">
+              <Switch>
+                <Route exact={true} path="/" component={Dashboard} />
+                <Route exact={true} path="/tables" component={Table} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom"
+function Sidebar() {
+    return (
+        <div>
+            <ul
+                class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+                id="accordionSidebar"
+            >
+                <a
+                    class="sidebar-brand d-flex align-items-center justify-content-center"
+                    href="index.html"
+                >
+                    <div class="sidebar-brand-icon rotate-n-15">
+                        <i class="fas fa-laugh-wink"></i>
+                    </div>
+                    <div class="sidebar-brand-text mx-3">
+                        SB Admin <sup>2</sup>
+                    </div>
+                </a>
+
+                <hr class="sidebar-divider my-0" />
+
+                <li class="nav-item active">
+                    <Link class="nav-link" to="/">
+                        <i class="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Dashboard</span>
+                    </Link>
+                </li>
+
+                <li class="nav-item">
+                    <Link class="nav-link" to="/tables">
+                        <i class="fas fa-fw fa-table"></i>
+                        <span>Tables</span>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
